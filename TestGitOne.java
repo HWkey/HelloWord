0eb4ee407dc3692862f1e6f9704fc1b1142879c8
Jin Kai <752086348@qq.com>
2019-04-25 09:44:26 +0800

    This is The SeondTry

---
+++ TestGitOne.java
@@ -0,0 +1,11 @@
+package findOne;
+
+public class TestGitOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the Test!");
+	}
+
+}
